2c16f41875e39bc557281837cfd697814d08f0f5
palakdevgan <m7.palak@gmail.com>
2022-04-13 10:49:32 -0400

    Modified HTML Template to handle more data

--- app.js
+++ app.js
@@ -154,44 +154,14 @@ return inquirer.prompt([
 });
 };
 
-const mockData = {
-    name: 'Lernantino',
-    github: 'lernantino',
-    confirmAbout: true,
-    about:
-      'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et.',
-    projects: [
-      {
-        name: 'Run Buddy',
-        description:
-          'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
-        languages: ['HTML', 'CSS'],
-        link: 'https://github.com/lernantino/run-buddy',
-        feature: true,
-        confirmAddProject: true
-      },
-      {
-        name: 'Taskinator',
-        description:
-          'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
-        languages: ['JavaScript', 'HTML', 'CSS'],
-        link: 'https://github.com/lernantino/taskinator',
-        feature: true,
-        confirmAddProject: true
-      }
-    ]
-  };
-
-  const pageHTML = generatePage(mockData);
-
-// promptUser()
-// .then(promptProject)
-// .then(portfolioData => {
-//     const pageHTML = generatePage(portfolioData);
+ promptUser()
+ .then(promptProject)
+ .then(portfolioData => {
+     const pageHTML = generatePage(portfolioData);
 
  fs.writeFile('./index.html', pageHTML, err => {
    if (err) throw err;
 
    console.log('Portfolio complete! Check out index.html to see the output!');
 });
-// });
+ });
